83bac24804555ed0ca8269ddedaf6e4dd0f28434
mrtnvgr <martynovegorOF@yandex.ru>
2026-01-26 09:46:09 +0700

    feat: add `--all` cli argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fetchers.ilibrary import fetch_ilibrary
+from argparse import ArgumentParser
 from iterfzf import iterfzf
 import pickle
 import shutil
@@ -9,11 +10,18 @@ if not shutil.which("pandoc"):
     print("Pandoc is required for this :)")
     sys.exit(1)
 
+parser = ArgumentParser()
+parser.add_argument("--all", action="store_true", help="Download everything")
+args = parser.parse_args()
+
 with open("db.pickle", "rb") as f:
     db = pickle.load(f)
 
-prompt = "(Press Tab for multi-select): "
-books = iterfzf(sorted(db.keys()), cycle=True, multi=True, prompt=prompt)
+if args.all:
+    books = db.keys()
+else:
+    prompt = "(Press Tab for multi-select): "
+    books = iterfzf(db.keys(), cycle=True, multi=True, prompt=prompt)
 
 for book in books:
     print(f"Downloading \"{book}\"...")
@@ -25,3 +33,5 @@ for book in books:
         fetch_ilibrary(meta, f"{book}.epub")
     else:
         raise Exception(f"invalid source: {source}")
+
+print("Done!")
