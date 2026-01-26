e1cf3de8f00756a1eb74988c966ce0d18646363e
mrtnvgr <martynovegorOF@yandex.ru>
2026-01-26 19:00:37 +0700

    feat(web): generate index

--- web/generate.py
+++ web/generate.py
@@ -1,7 +1,42 @@
+import pickle
 import os
 
 os.makedirs("data", exist_ok=True)
 
 # TODO: move all epubs from ../ to data
 
-# TODO: generate an html page
+with open("../db.pickle", "rb") as f:
+    db = pickle.load(f)
+
+template = lambda books: f"""
+<!DOCTYPE html>
+<html lang="ru" translate="no">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Книги</title>
+
+		<link rel="stylesheet" href="styles.css">
+		<script src="script.js"></script>
+	</head>
+
+	<body>
+		<input type="text" id="query" placeholder="Поиск..." autocomplete="off">
+
+		<ul id="books">{books}</ul>
+	</body>
+</html>
+"""
+
+# <li class="book"><a href="./test2">А. Достоевский - Тестбук1</a></li>
+# <li class="book"><a href="./test">А. Петров - Тестобуко9</a></li>
+
+books = [
+    f'<li class="book"><a href="./UNKNOWNYETSORRY">{name}</a></li>'
+    for name in db.keys()
+]
+books = "".join(books)
+
+index = template(books)
+with open("index.html", "w") as f:
+    f.write(index)
--- web/script.js
+++ web/script.js
@@ -1,14 +1,20 @@
-function search() {
-    const raw_query = document.getElementById("query").value;
-    const query = raw_query.toLowerCase().trim();
+document.addEventListener("DOMContentLoaded", function() {
+    const books = Array.from(document.querySelectorAll("ul#books li"));
 
-    const empty_query = query.length == 0;
+    function search() {
+        const raw_query = document.getElementById("query").value;
+        const query = raw_query.toLowerCase().trim();
+        const empty_query = query.length === 0;
 
-    const books = document.querySelectorAll("ul#books li");
-    for (book of books) {
-        const book_name = book.innerHTML.toLowerCase();
-
-        const match = book_name.includes(query) || empty_query;
-        book.style.display = match ? "" : "none";
+        requestAnimationFrame(() => {
+            for (let book of books) {
+                const book_name = book.textContent.toLowerCase();
+                const match = book_name.includes(query) || empty_query;
+                book.style.display = match ? "" : "none";
+            }
+        })
     }
-}
+
+    const query = document.getElementById("query");
+    query.addEventListener("input", search);
+})
