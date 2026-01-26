149d10c415e79f7a2b669dcf5928c27437a65670
mrtnvgr <martynovegorOF@yandex.ru>
2026-01-26 09:47:29 +0700

    fix: use `__name__ == "__main__"` pattern

--- main.py
+++ main.py
@@ -6,32 +6,36 @@ import shutil
 import sys
 import os
 
-if not shutil.which("pandoc"):
-    print("Pandoc is required for this :)")
-    sys.exit(1)
+def main():
+    if not shutil.which("pandoc"):
+        print("Pandoc is required for this :)")
+        sys.exit(1)
 
-parser = ArgumentParser()
-parser.add_argument("--all", action="store_true", help="Download everything")
-args = parser.parse_args()
+    parser = ArgumentParser()
+    parser.add_argument("--all", action="store_true", help="Download everything")
+    args = parser.parse_args()
 
-with open("db.pickle", "rb") as f:
-    db = pickle.load(f)
+    with open("db.pickle", "rb") as f:
+        db = pickle.load(f)
 
-if args.all:
-    books = db.keys()
-else:
-    prompt = "(Press Tab for multi-select): "
-    books = iterfzf(db.keys(), cycle=True, multi=True, prompt=prompt)
-
-for book in books:
-    print(f"Downloading \"{book}\"...")
-
-    meta = db[book]
-
-    # TODO: convert meta to dataclass, meta.source to enum
-    if meta["source"] == "ilibrary":
-        fetch_ilibrary(meta, f"{book}.epub")
+    if args.all:
+        books = db.keys()
     else:
-        raise Exception(f"invalid source: {source}")
+        prompt = "(Press Tab for multi-select): "
+        books = iterfzf(db.keys(), cycle=True, multi=True, prompt=prompt)
 
-print("Done!")
+    for book in books:
+        print(f"Downloading \"{book}\"...")
+
+        meta = db[book]
+
+        # TODO: convert meta to dataclass, meta.source to enum
+        if meta["source"] == "ilibrary":
+            fetch_ilibrary(meta, f"{book}.epub")
+        else:
+            raise Exception(f"invalid source: {source}")
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
