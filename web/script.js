5cbfd49d35711aec1692a68dca144dbe56f66469
mrtnvgr <martynovegorOF@yandex.ru>
2026-02-18 17:22:31 +0700

    feat: new search web interface

--- web/script.js
+++ web/script.js
@@ -1,27 +1,76 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const FzF = window.fzf.Fzf;
+function display(elem, value) {
+    elem.style.display = value ? "revert-layer" : "none";
+}
 
-    const books = Array.from(document.querySelectorAll("ul#books li a"));
+async function getFileContents(url_path) {
+    const host = window.location.host;
+    const protocol = window.location.protocol;
+    const url = `${protocol}//${host}/${url_path}`;
 
-    const fzf = new FzF(books, {
-      selector: (item) => item.textContent,
-    });
+	const response = await fetch(url);
+	return await response.text();
+}
 
-    function search() {
-        const raw_query = document.getElementById("query").value;
-        const results = fzf.find(raw_query);
-
-        requestAnimationFrame(() => {
-            for (let book of books) {
-                book.style.display = "none";
-            }
-
-            for (let result of results) {
-                result.item.style.display = "";
-            }
-        })
+async function fetchDB() {
+    const cached = sessionStorage.getItem("db");
+    if (cached) {
+        return JSON.parse(cached);
     }
 
-    const search_bar = document.getElementById("query");
-    search_bar.addEventListener("input", search);
+    const raw = await getFileContents("db.json");
+    sessionStorage.setItem("db", raw);
+
+    return JSON.parse(raw);
+}
+
+async function search(query) {
+    const db = await fetchDB();
+
+    const fzf = new window.fzf.AsyncFzf(Object.entries(db), {
+      selector: (x) => x[0],
+    });
+
+    const books = await fzf.find(query);
+
+    const booksList = document.getElementById("books");
+
+    for (const book of books) {
+        const [name, data] = book.item;
+
+        const li = document.createElement("li");
+        li.className = "book";
+
+        const a = document.createElement("a");
+        // TODO: strip name to 200 chars
+        a.href = `./d/${encodeURI(name)}.epub`;
+        a.textContent = name;
+
+        li.appendChild(a);
+        booksList.appendChild(li);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const params = new URLSearchParams(window.location.search);
+    const query = params.get("q") || "";
+
+    let elems = document.querySelectorAll("input#query");
+    for (let elem of elems) {
+        elem.value = query;
+    }
+
+    let screen;
+
+    const showSearchResults = query.length > 0;
+
+    if (showSearchResults) {
+        screen = "form#results";
+        search(query);
+    } else {
+        screen = "form#search";
+    }
+
+    requestAnimationFrame(() => {
+        display(document.querySelector(screen), true);
+    });
 })
